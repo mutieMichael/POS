6ce5884398d29645b52ab088435690e11eb5ed8d
mutieMichael <silentguru1994@gmail.com>
2023-03-08 20:58:50 +0300

    added inventory and transactions excel files

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import dash
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dash_table
+from dash import dcc
+from dash import html
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import pandas as pd
